76532a65b4c3dad8e26d683078d5b32e9a6219c5
Nikita Maslov <nikita.maslov@veeam.com>
2019-12-04 16:28:23 +0300

    t

---
+++ SharedTask.Tests/SharedQueuedTaskTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SharedTask.Tests
+{
+    [TestFixture]
+    public class SharedQueuedTaskTests
+    {
+    }
+}
---
+++ SharedTask/SharedQueuedTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharedTask
+{
+    public sealed class SharedQueuedTask<T> : IDisposable
+    {
+        public Task<T> GetOrCreateAsync(CancellationToken cancellationToken = default)
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public sealed class SharedQueuedTask : IDisposable
+    {
+        public Task GetOrCreateAsync(CancellationToken cancellationToken = default)
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
