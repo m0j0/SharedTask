2fd91db3916dc51455b7e8f3add7e679f2d64d36
Nikita Maslov <nikita.maslov@veeam.com>
2019-12-04 16:34:56 +0300

    t

--- SharedTask/SharedQueuedTask.cs
+++ SharedTask/SharedQueuedTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,6 +7,14 @@ namespace SharedTask
 {
     public sealed class SharedQueuedTask<T> : IDisposable
     {
+        public SharedQueuedTask(Func<CancellationToken, Task<T>> getTask)
+        {
+        }
+
+        public SharedQueuedTask(Func<Task<T>> getTask) : this(token => getTask())
+        {
+        }
+
         public Task<T> GetOrCreateAsync(CancellationToken cancellationToken = default)
         {
             return null;
@@ -18,6 +27,14 @@ namespace SharedTask
 
     public sealed class SharedQueuedTask : IDisposable
     {
+        public SharedQueuedTask(Func<CancellationToken, Task> getTask)
+        {
+        }
+
+        public SharedQueuedTask(Func<Task> getTask)
+        {
+        }
+
         public Task GetOrCreateAsync(CancellationToken cancellationToken = default)
         {
             return null;
